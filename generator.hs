d68e3313c939b539c79692c945ce9ebb84b24918
oisincar <oisincar@gmail.com>
2017-02-14 12:12:51 +0000

    Add some comments & clean up code

--- generator.hs
+++ generator.hs
@@ -7,10 +7,8 @@ main = do
   args <- getArgs
   let b = read (args !! 0)
       h = read (args !! 1)
-  -- let b = 5
-  --     h = 9
 
-  let allN = getAllNodes b h
+      allN = getAllNodes b h
       revGr = revGraph $ graph b allN
       borderLine = '|' : (replicate (2 * length allN -1) '-') ++ "|"
 
@@ -22,52 +20,59 @@ main = do
 
   -- special case for base throw
   putStrLn $ "|^" ++ (concat $ replicate (length allN) "| ")
-  putStrLn $ intercalate "\n" $
-    [showLine (revGr !! x) x (length revGr) | x <- [0..(length revGr) -1]]
+  putStrLn $ intercalate "\n" $ showGraph revGr
   putStrLn borderLine
 
+--   num balls -> throw height -> [state strings]
+getAllNodes :: Int -> Int -> [String]
 getAllNodes b h = reverse $ sort $ nub $ permutations ((replicate b '1') ++ (replicate (h-b) '0'))
-
-findNodes :: Int -> String -> [(Int, String)]
-findNodes b (x:xs)
- | x == '0' = [(0, xs ++ "0")]
- | otherwise = filter isValid $ map (\i -> (i+1, replace (xs ++ "0") i)) [0..length xs]
- where isValid (_,st) = (b == length (filter (=='1') st))
-       replace str x = (take x str) ++ "1" ++ (drop (x+1) str)
 
 --     num balls -> all states -> [start state,  [(throwheight, end state)]]
 graph :: Int -> [String] -> [(String, [(Int, String)])]
-graph b allNodes = map (\x -> (x, findNodes b x)) allNodes
+graph b allNodes = zip allNodes $ map findNodes allNodes
+  where
+    findNodes :: String -> [(Int, String)]
+    findNodes (x:xs)
+     | x == '0' = [(0, xs ++ "0")]
+     | otherwise = filter isValid $ map (\i -> (i+1, replace (xs ++ "0") i)) [0..length xs]
+     where isValid (_,st) = (b == length (filter (=='1') st))
+           replace str x = (take x str) ++ "1" ++ (drop (x+1) str)
 
---          graph               ->         index, throwHeight
-revGraph :: [(String, [(Int, String)])] ->  [[(Int, Int)]]
+
+--                      graph           -> [[index, throwHeight]]
+revGraph :: [(String, [(Int, String)])] -> [[(Int, Int)]]
 revGraph gr = map f gr
-  where --f :: (String, [(Int, String)]) -> [(Int, Int)]
-        f node = g gr (fst node) 0
-        -- g :: [(String, [(Int, String)])] -> String -> Int -> [(Int, Int)]
-        g [] _ _ = []
-        g (x:xs) s ix
-          | isJust matchState = (ix, (fst $ fromJust matchState)) : g xs s (ix+1)
-          | otherwise = g xs s (ix+1)
-          where matchState = find (\y -> snd y == s) (snd x)
+  where
+    f :: (String, [(Int, String)]) -> [(Int, Int)]
+    f node = g gr (fst node) 0
+    g :: [(String, [(Int, String)])] -> String -> Int -> [(Int, Int)]
+    g [] _ _ = []
+    g (x:xs) s ix
+      | isJust matchState = (ix, (fst $ fromJust matchState)) : g xs s (ix+1)
+      | otherwise = g xs s (ix+1)
+      where matchState = find (\y -> snd y == s) (snd x)
 
+--    throwHeight -> [states] -> [lines]
+showStates :: Int -> [String] -> [String]
 showStates h st = (map showLine (reverse [0..h-1]))
   where showLine y = '|' : concatMap (\s -> (s !! y) : "|") st
 
--- [(ix, throwHeight)], end ix, width
-showLine :: [(Int, Int)] -> Int -> Int -> String
-showLine arr ix width = '|' : (replicate (stIx*2) ' ')
-                         ++ (drop 1 $ g arr stIx)
-                         ++ (concat $ replicate (width - endIx) "| ")
-  where stIx  = min ix $ fst (head arr)
-        endIx = max ix $ fst (last arr)
-        g lst i
-          | i > endIx = ""
-          | lst /= [] && fst (head lst) == i =
-            arrowS : (showThrow $ snd (head lst)) ++ g (tail lst) (i+1)
-          | i == ix   = arrowS : "." ++ rec
-          | otherwise = arrowS : "-" ++ rec
-          where rec = g lst (i+1)
-                arrowS | i > ix = '<' | otherwise = '>'
-                showThrow x | x < 10 = (show x)
-                            | otherwise = chr (x + 87) : ""
+
+showGraph revGr = [showLine (revGr !! x) x (length revGr) | x <- [0..(length revGr) -1]]
+  where
+    showLine :: [(Int, Int)] -> Int -> Int -> String
+    showLine arr ix width = '|' : (replicate (stIx*2) ' ')
+                             ++ (drop 1 $ g arr stIx)
+                             ++ (concat $ replicate (width - endIx) "| ")
+      where stIx  = min ix $ fst (head arr)
+            endIx = max ix $ fst (last arr)
+            g lst i
+              | i > endIx = ""
+              | lst /= [] && fst (head lst) == i =
+                arrowS : (showThrow $ snd (head lst)) ++ g (tail lst) (i+1)
+              | i == ix   = arrowS : "." ++ rec
+              | otherwise = arrowS : "-" ++ rec
+              where rec = g lst (i+1)
+                    arrowS | i > ix = '<' | otherwise = '>'
+                    showThrow x | x < 10 = (show x)
+                                | otherwise = chr (x + 87) : ""
